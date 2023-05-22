dbdee706f8d222aa82c55f64a9d70adaeeced27e
baranysf <baranrise@gmail.com>
2023-05-23 04:08:36 +0900

    proje tamamlandı

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log('Hello from Console');
+function birthyear2age(){
+    event.preventDefault();
+
+
+    var birth_year;
+    var current_year;
+    var age;
+
+    
+    var d=new Date();
+    current_year = d.getFullYear();
+
+    birth_year = document.getElementById('birth_year').value;
+
+    
+    
+    age = current_year - birth_year;
+
+    console.log(age);
+
+    document.getElementById('result').innerHTML = "Yaşınız:" + age;
+
+}
